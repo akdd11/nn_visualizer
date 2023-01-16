8cbcb6f745f2200128c2ff83b3ead1d8a5fc754b
Anton Krause <anton.krause@ifn.et.tu-dresden.de>
2023-01-16 16:10:37 +0100

    added license

--- src/nn_visualizer.py
+++ src/nn_visualizer.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2023, Anton Krause
+# All rights reserved.
+
+# This source code is licensed under the GPL-3 license found in the
+# LICENSE file in the root directory of this source tree. 
+
 import matplotlib.pyplot as plt
 import numpy as np
 
